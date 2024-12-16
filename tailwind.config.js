da7c86a85eaf79daab1662721bf03b249077ba19
Luki riski <contactlukiriski@gmail.com>
2024-12-17 01:16:31 +0700

    Complete login page slicing

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Lato', 'sans-serif'],
+            },
+            colors: {
+                customWhite: '#FAFAFA',
+                greenPrimary: "#3ECF4C",
+                greenPrimary100: "#E2FCD9CC",
+                backgroundPrimary: "#fffdf3",
+                yellowColorButton: "#FFBD3A",
+                orangeColorPrimary: "#F64920",
+                textColorBlack: "#282424",
+                textColorsecond: "#747474",
+            },
+        },
+    },
+    content: ['./*.html',],
+}
